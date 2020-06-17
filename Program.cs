c9de8535e03e34f5a1de02da12f49db1b6e2f4b2
diego_neri <diego@diegoneri.com>
2020-06-17 18:40:43 -0300

    Adiciona conteudo apos _dotnet new console_

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
